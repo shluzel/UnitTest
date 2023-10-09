3c326d3d47f9207c609d34dcb16cf264c995a444
Darya <dasha.890@yandex.ru>
2023-10-09 18:49:06 +0500

    pravki

--- HW03/test/NumbersTest.java
+++ HW03/test/NumbersTest.java
@@ -1,30 +1,33 @@
 package HW03.test;
 
 import org.junit.jupiter.api.Test;
+
+import HW03.Numbers;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NumbersTest {
     @Test
     void evenOddNumberTrue() {
-        boolean t = Main.evenOddNumber(8);
+        boolean t = Numbers.evenOddNumber(8);
         assertEquals(t, true);
 
     }
     @Test
     void evenOddNumberFalse() {
-        boolean f = Main.evenOddNumber(13);
+        boolean f = Numbers.evenOddNumber(13);
         assertEquals(f, false);
     }
 
     @Test
     void numberInIntervalTrue() {
-        boolean t = Main.numberInInterval(45);
+        boolean t = Numbers.numberInInterval(45);
         assertEquals(t, true);
     }
 
     @Test
     void numberInIntervalFalse() {
-        boolean f = Main.numberInInterval(101);
+        boolean f = Numbers.numberInInterval(101);
         assertEquals(f, false);
     }
 }
